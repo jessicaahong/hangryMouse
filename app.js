e81edfb98546f2101a0fb115d55cbfcc1232b34e
jessicaahong <jessiehong9@gmail.com>
2016-02-02 13:59:41 -0800

    writes initial HTML and CSS, just starts working on app.js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+function init(){
+	//things that you want to happen once page loads
+	console.log('working');
+	loadClickEventListeners();
+}
 
+function loadClickEventListeners() {
+	$('#wharf').on('click', mouseJump);
+}
+
+function mouseJump() {
+	$('#mouse').css();
+
+}
+
+  var mousePosition = $('#mouse').css(["top", "right"]);
+  $.each( mousePosition, function( prop, value ) {
+    html.push( prop + ": " + value )});
+
+  var newMousePosition = $('#wharf').css({'top','right'});
