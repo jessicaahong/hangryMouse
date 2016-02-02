2d930efb73911835c9f6d62a823442b0eca9c118
jessicaahong <jessiehong9@gmail.com>
2016-02-02 15:33:30 -0800

    gets click add listener function to operate for all munistation IDs

--- app.js
+++ app.js
@@ -1,20 +1,29 @@
 function init(){
 	//things that you want to happen once page loads
 	console.log('working');
-	loadClickEventListeners();
+	$('button').on('click', newGame);
+	//eventually run functions that add animations to mouse (nose twitch)
 }
+
+function newGame() {
+	loadClickEventListeners();
+	//run function that
+		//has mouse fall on to new location
+		//has prize appear at new location
+}
+
 
 function loadClickEventListeners() {
-	$('#wharf').on('click', mouseJump);
+	$('.munistop').on('click', function() {
+		mouseJump(event.target);
+	});
 }
 
-function mouseJump() {
-	$('#mouse').css();
 
+function mouseJump(targetElement) {
+	var latNum = (parseInt($(targetElement).css('top'))-15);
+	var newMouseLatitude = latNum.toString() + "px";
+	var longNum = (parseInt($(targetElement).css('right'))-12);
+	var newMouseLongitude = longNum.toString() + "px";
+	$('#mouse').css({'top' : newMouseLatitude, 'right' : newMouseLongitude});
 }
-
-  var mousePosition = $('#mouse').css(["top", "right"]);
-  $.each( mousePosition, function( prop, value ) {
-    html.push( prop + ": " + value )});
-
-  var newMousePosition = $('#wharf').css({'top','right'});
