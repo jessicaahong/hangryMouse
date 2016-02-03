4fd530234c3ae50b4cffef63eda23783f6cda363
jessicaahong <jessiehong9@gmail.com>
2016-02-02 19:44:03 -0800

    edits so mouse can only move to muni stations within the same train line

--- app.js
+++ app.js
@@ -31,6 +31,24 @@ function mouseJump(targetElement) {
 	//shift position 8 pixels up so you center the mouse around the node
 	var longNum = (parseInt($(targetElement).css('right'))-8);
 	var newMouseLongitude = longNum.toString() + "px";
-	//set mouse position so that it now appears at the clicked muni station
-	$('#mouse').css({'top' : newMouseLatitude, 'right' : newMouseLongitude});
+	//only move mouse if mouse and new muni stop are part of the same line
+	if (($('#mouse').hasClass('redLine') && $(targetElement).hasClass('redLine')) ||
+		($('#mouse').hasClass('orangeLine') && $(targetElement).hasClass('orangeLine')) ||
+		($('#mouse').hasClass('yellowLine') && $(targetElement).hasClass('yellowLine')) ||
+		($('#mouse').hasClass('greenLine') && $(targetElement).hasClass('greenLine')) ||
+		($('#mouse').hasClass('blueLine') && $(targetElement).hasClass('blueLine')) ||
+		($('#mouse').hasClass('purpleLine') && $(targetElement).hasClass('purpleLine'))) {
+			//set mouse position so that it now appears at the clicked muni station
+			$('#mouse').css({'top' : newMouseLatitude, 'right' : newMouseLongitude});
+			//create variable that holds all classes from new muni stop, which we will later assign to mouse
+			var classList = targetElement.classList.toString();
+			//remove all previously set classes from mouse with .removeClass()
+			$('#mouse').removeClass();
+			//add line classes of new location to mouse with .addClass()
+			$('#mouse').addClass(classList);
+	//else if they do not belong to the same line, alert that they must go to transfer station
+	} else {
+		alert('you must visit a transfer stations before switching muni lines!');
+	}
+
 }
