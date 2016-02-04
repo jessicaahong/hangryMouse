24af1f909726452b9eb1c94a6c6597fa3b608aa8
jessicaahong <jessiehong9@gmail.com>
2016-02-04 14:49:53 -0800

    makes game fully functional with win logic and makes win messages appear, properly formatted. also adds media queries for sidebar

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ var arrayOfFunctions = [
 			setSushiAdventure];
 var winningStation;
 var currentStation;
+var yelpLink;
 
 function newGame() {
 	hasStarted = true;
@@ -57,7 +58,9 @@ function letMouseWin(targetElement){
 }
 
 function showWinnerMessage(){
-	$('#adventureDetails').html("Congratulations! You have won the game!<br>Click <a href='#'>here</a> to find out more about restaurants in the neighborhood!<br>Click new game to send your mouse on another adventure!");
+	$('#adventureDetails').html("<span id='congratulations'>Congratulations! You have won the game!</span><br>Click <a id='yelpLink' target='_blank' href='#'>here</a> to find out about other restaurants in the area!<br>Click new game to send your mouse on another adventure!");
+	$('#congratulations').css({'color' : '#EA242F'});
+	$('#yelpLink').attr('href', yelpLink);
 }
 
 function completeMove(targetElement) {
@@ -103,6 +106,8 @@ function invalidMoveAlert() {
 }
 
 function selectNewAdventure() {
+	//make insructions black again
+	// $('#adventureDetails').css({'color' : 'balck'});
 	for (var i = arrayOfFunctions.length-1; i >= 0; i--) {
 		if (hasStarted) {
 			arrayOfFunctions[i]();
@@ -130,92 +135,105 @@ function selectNewAdventure() {
 // adventure functions below
 
 function setBeerAdventure() {
-	$('#adventureDetails').html('Direct mouse to dogpatch to grab some beers!');
-	winningStation = document.querySelector('#dogpatch');	
+	$('#adventureDetails').html('<br>Direct mouse to Dogpatch to grab some beers with friends!');
+	winningStation = document.querySelector('#dogpatch');
+	yelpLink = 'http://www.yelp.com/search?find_desc=restaurants&find_loc=Dogpatch,+San+Francisco,+CA';
 	moveMouse('#nineteenthSt');
 	adjustClasses(document.querySelector('#nineteenthSt'));
 	$('#beer').css({'display': 'block'});
 }
 function setBrownieAdventure() {
-	$('#adventureDetails').html('Direct mouse to the Haight to pick up some brownies!');
+	$('#adventureDetails').html('<br>Direct mouse to the Haight to pick up some brownies!');
 	winningStation = document.querySelector('#haightAshbury');
+	yelpLink = 'http://www.yelp.com/search?find_desc=restaurants&find_loc=San+Francisco,+CA&start=0&l=g:-122.43932247161865,37.77416323052168,-122.45219707489014,37.76398604059808';
 	moveMouse('#hudson');
 	adjustClasses(document.querySelector('#hudson'));
 	$('#brownie').css({'display': 'block'});
 }
 function setBurgerAdventure() {
-	$('#adventureDetails').html('Direct mouse to dividadero street for a NOPA burger!');
+	$('#adventureDetails').html('<br>Direct mouse to Divisadero for a NOPA burger!');
 	winningStation = document.querySelector('#divisadero');
+	yelpLink = 'http://www.yelp.com/search?find_desc=restaurants&find_loc=NoPa,+San+Francisco,+CA&start=0&l=g:-122.43170499801636,37.78258620984929,-122.44457960128784,37.77241017934441';
 	moveMouse('#glenPark');
 	adjustClasses(document.querySelector('#glenPark'));
 	$('#burger').css({'display': 'block'});
 }
 function setBurritoAdventure() {
-	$('#adventureDetails').html('Direct mouse to 24th St for la taqueria!');
+	$('#adventureDetails').html('<br>Direct mouse to 24th St for a La Taqueria burrito!');
 	winningStation = document.querySelector('#twentyFourthSt');
+	yelpLink = 'http://www.yelp.com/search?find_desc=burrito&find_loc=Mission,+San+Francisco,+CA';
 	moveMouse('#landsEnd');
 	adjustClasses(document.querySelector('#landsEnd'));
 	$('#burrito').css({'display': 'block'});
 }
 function setCheeseAdventure() {
-	$('#adventureDetails').html('Direct mouse to 16th St to get some cheese at BiRite!');
+	$('#adventureDetails').html('<br>Direct mouse to 16th St to pick up some cheese at Bi-Rite!');
 	winningStation = document.querySelector('#sixteenthSt');
+	yelpLink = 'http://www.yelp.com/search?find_desc=restaurants&find_loc=Mission,+San+Francisco,+CA&start=0&l=g:-122.41496801376343,37.766980290256306,-122.42784261703491,37.756802111778526';
 	moveMouse('#union');
 	adjustClasses(document.querySelector('#union'));
 	$('#cheese').css({'display': 'block'});
 }
 function setChowderAdventure() {
-	$('#adventureDetails').html('Direct mouse to the wharf for some clam chowder!');
+	$('#adventureDetails').html('<br>Direct mouse to Fisherman\'s Wharf for some clam chowder!');
 	winningStation = document.querySelector('#wharf');
+	yelpLink = 'http://www.yelp.com/search?find_desc=chowder&find_loc=Fisherman%27s+Wharf,+San+Francisco,+CA';
 	moveMouse('#bayview');
 	adjustClasses(document.querySelector('#bayview'));
 	$('#chowder').css({'display': 'block'});
 }
 function setCoffeeAdventure() {
-	$('#adventureDetails').html('Direct mouse the castro for a cup of coffee!');
+	$('#adventureDetails').html('<br>Direct mouse the Castro for a cup of fair trade coffee!');
 	winningStation = document.querySelector('#castro');
+	yelpLink = 'http://www.yelp.com/search?find_desc=restaurants&find_loc=Castro,+San+Francisco,+CA,+USA';
 	moveMouse('#union');
 	adjustClasses(document.querySelector('#union'));
 	$('#coffee').css({'display': 'block'});
 }
 function setCrabAdventure() {
-	$('#adventureDetails').html('Direct mouse Land\'s End for some seafood!');
+	$('#adventureDetails').html('<br>Direct mouse Land\'s End for some seafood!');
 	winningStation = document.querySelector('#landsEnd');	
+	yelpLink = 'http://www.yelp.com/search?find_desc=restaurants&find_loc=san+francisco&start=0&l=g:-122.49176502227783,37.78369529480428,-122.5175142288208,37.7633421384456';
 	moveMouse('#designDistrict');
 	adjustClasses(document.querySelector('#designDistrict'));
 	$('#crab').css({'display': 'block'});
 }
 function setDimsumAdventure() {
-	$('#adventureDetails').html('Direct mouse the richmond for some dim sum!');
-	winningStation = document.querySelector('#theAvenues');	
+	$('#adventureDetails').html('<br>Direct mouse the Outer Richmond for some dim sum!');
+	winningStation = document.querySelector('#theAvenues');
+	yelpLink = 'http://www.yelp.com/search?find_desc=dim+sum&find_loc=san+francisco&start=0&l=g:-122.47189521789551,37.78925681551992,-122.49764442443848,37.76890519045137';	
 	moveMouse('#civicCenter');
 	adjustClasses(document.querySelector('#civicCenter'));
 	$('#dimsum').css({'display': 'block'});
 }
 function setHotdogAdventure() {
-	$('#adventureDetails').html('Direct mouse to the ballpark for a hot dog!');
+	$('#adventureDetails').html('<br>Direct mouse to the ballpark for a hot dog!');
 	winningStation = document.querySelector('#fourthAndKing');
+	yelpLink = 'http://www.yelp.com/search?find_desc=restaurants&find_loc=San+Francisco,+CA&start=0&l=g:-122.37825393676758,37.790105203510535,-122.40400314331055,37.769753812051114';
 	moveMouse('#balboaPark');
 	adjustClasses(document.querySelector('#balboaPark'));
 	$('#hotdog').css({'display': 'block'});
 }
 function setPizzaAdventure() {
-	$('#adventureDetails').html('Direct mouse to North Beach for a slice of pizza!');
+	$('#adventureDetails').html('<br>Direct mouse to North Beach for a slice of pizza!');
 	winningStation = document.querySelector('#northBeach');
+	yelpLink = 'http://www.yelp.com/search?find_desc=pizza&find_loc=North+Beach,+San+Francisco,+CA';
 	moveMouse('#taraval');
 	adjustClasses(document.querySelector('#taraval'));
 	$('#pizza').css({'display': 'block'});
 }
 function setPretzelAdventure() {
-	$('#adventureDetails').html('Direct mouse to Stonestown Galleria for a soft pretzel!');
+	$('#adventureDetails').html('Direct mouse to Stonestown Galleria for a soft pretzel! <br>Who doesn\'t love soft pretzels??');
 	winningStation = document.querySelector('#stonestown');
+	yelpLink = 'http://www.yelp.com/search?find_desc=restaurants&find_loc=San+Francisco,+CA&start=0&l=g:-122.44945049285889,37.74243970293853,-122.50094890594482,37.70170508361123';
 	moveMouse('#candlestick');
 	adjustClasses(document.querySelector('#candlestick'));
 	$('#pretzel').css({'display': 'block'});
 }
 function setSushiAdventure(){
-	$('#adventureDetails').html('Direct mouse to Japantown for some sushi!');
+	$('#adventureDetails').html('<br>Direct mouse to Japantown for some sushi!');
 	winningStation = document.querySelector('#japantown');
+	yelpLink = 'http://www.yelp.com/search?find_desc=sushi&find_loc=Japantown%2C+San+Francisco%2C+CA&ns=1';
 	moveMouse('#stonestown');
 	adjustClasses(document.querySelector('#stonestown'));
 	$('#sushi').css({'display' : 'block'});	
