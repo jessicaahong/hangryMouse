1a373fbb118e0d855cfd160f6e3a2be73d6e411d
jessicaahong <jessiehong9@gmail.com>
2016-02-03 21:42:46 -0800

    adds food/prize images to map, adds functionality to newgame button, adds more adventure options

--- app.js
+++ app.js
@@ -1,79 +1,199 @@
 function init(){
-	//things that you want to happen once page loads
-	//make newgame button clickable so you can start a new game
 	$('button').on('click', newGame);
 }
 
+var hasStarted = false;
+var arrayOfFunctions = [
+			setBeerAdventure,
+			setBrownieAdventure,
+			setBurgerAdventure,
+			setBurritoAdventure,
+			setCheeseAdventure,
+			setChowderAdventure,
+			setCoffeeAdventure,
+			setCrabAdventure,
+			setDimsumAdventure,
+			setHotdogAdventure,
+			setPizzaAdventure,
+			setPretzelAdventure,
+			setSushiAdventure];
+
 function newGame() {
+	hasStarted=true;
+	//make exclamation point invisible with new game
+	$('#exclamationPointDiv').css({'display' : 'none'});
+	//make all food icons invisible with new game
+	$('.food').css({'display': 'none'});
 	//make nodes clickable
 	loadClickEventListeners();
 	//select new gameboard randomly, and generates new gameboard
 	selectRandomAdventure();
-	setupAdventureOnMap();
+	//then display mouse at new location
+	$('#mouse').css({'display' : 'block', 'position' : 'absolute'});
 }
 
 function selectRandomAdventure() {
-
+	for (var i = arrayOfFunctions.length-1; i >= 0; i--) {
+		if (hasStarted) {
+			arrayOfFunctions[i]();
+			arrayOfFunctions.pop();
+			hasStarted = false;
+			console.log(arrayOfFunctions);
+		}
+		if(arrayOfFunctions.length===0) {
+			arrayOfFunctions.push(setBeerAdventure,
+				setBrownieAdventure,
+				setBurgerAdventure,
+				setBurritoAdventure,
+				setCheeseAdventure,
+				setChowderAdventure,
+				setCoffeeAdventure,
+				setCrabAdventure,
+				setDimsumAdventure,
+				setHotdogAdventure,
+				setPizzaAdventure,
+				setPretzelAdventure,
+				setSushiAdventure);
+		}
+	} 
 }
 
-function setupAdventureOnMap() {
-	//edit css to set mouse's new location (top and right css)
-	//display mouse at new location
-	$('#mouse').css({'display' : 'block', 'position' : 'absolute'});
-	//has prize appear at new location
-		//tailors adventureDetails text to new adventure
-		$('#adventureDetails').html('direct mouse to japantown for some sushi!');
-		//makes adventuresDetails text visible
-			$('#adventureDetails').css({'display': 'inline-block'});
-}
+// adventure functions below
 
+function setBeerAdventure() {
+	$('#adventureDetails').html('Direct mouse to dogpatch to grab some beers!');
+	moveMouse('#nineteenthSt');
+	adjustClasses(document.querySelector('#nineteenthSt'));
+	$('#beer').css({'display': 'block'});
+}
+function setBrownieAdventure() {
+	$('#adventureDetails').html('Direct mouse to the Haight to pick up some brownies!');
+	moveMouse('#hudson');
+	adjustClasses(document.querySelector('#hudson'));
+	$('#brownie').css({'display': 'block'});
+}
+function setBurgerAdventure() {
+	$('#adventureDetails').html('Direct mouse to dividadero street for a NOPA burger!');
+	moveMouse('#glenPark');
+	adjustClasses(document.querySelector('#glenPark'));
+	$('#burger').css({'display': 'block'});
+}
+function setBurritoAdventure() {
+	$('#adventureDetails').html('Direct mouse to 24th St for la taqueria!');
+	moveMouse('#landsEnd');
+	adjustClasses(document.querySelector('#landsEnd'));
+	$('#burrito').css({'display': 'block'});
+}
+function setCheeseAdventure() {
+	$('#adventureDetails').html('Direct mouse to 16th St to get some cheese at BiRite!');
+	moveMouse('#union');
+	adjustClasses(document.querySelector('#union'));
+	$('#cheese').css({'display': 'block'});
+}
+function setChowderAdventure() {
+	$('#adventureDetails').html('Direct mouse to the wharf for some clam chowder!');
+	moveMouse('#bayview');
+	adjustClasses(document.querySelector('#bayview'));
+	$('#chowder').css({'display': 'block'});
+}
+function setCoffeeAdventure() {
+	$('#adventureDetails').html('Direct mouse the castro for a cup of coffee!');
+	moveMouse('#union');
+	adjustClasses(document.querySelector('#union'));
+	$('#coffee').css({'display': 'block'});
+}
+function setCrabAdventure() {
+	$('#adventureDetails').html('Direct mouse Land\'s End for some seafood!');
+	moveMouse('#designDistrict');
+	adjustClasses(document.querySelector('#designDistrict'));
+	$('#crab').css({'display': 'block'});
+}
+function setDimsumAdventure() {
+	$('#adventureDetails').html('Direct mouse the richmond for some dim sum!');
+	moveMouse('#civicCenter');
+	adjustClasses(document.querySelector('#civicCenter'));
+	$('#dimsum').css({'display': 'block'});
+}
+function setHotdogAdventure() {
+	$('#adventureDetails').html('Direct mouse to the ballpark for a hot dog!');
+	moveMouse('#balboaPark');
+	adjustClasses(document.querySelector('#balboaPark'));
+	$('#hotdog').css({'display': 'block'});
+}
+function setPizzaAdventure() {
+	$('#adventureDetails').html('Direct mouse to North Beach for a slice of pizza!');
+	moveMouse('#taraval');
+	adjustClasses(document.querySelector('#taraval'));
+	$('#pizza').css({'display': 'block'});
+}
+function setPretzelAdventure() {
+	$('#adventureDetails').html('Direct mouse to Stonestown Galleria for a soft pretzel!');
+	moveMouse('#candlestick');
+	adjustClasses(document.querySelector('#candlestick'));
+	$('#pretzel').css({'display': 'block'});
+}
+function setSushiAdventure(){
+	$('#adventureDetails').html('Direct mouse to Japantown for some sushi!');
+	moveMouse('#stonestown');
+	adjustClasses(document.querySelector('#stonestown'));
+	$('#sushi').css({'display' : 'block'});	
+}
 
 //functions that make mouse mobile
 function loadClickEventListeners() {
 	$('.munistop').on('click', function() {
-		mouseJump(event.target);
+		completeMove(event.target);
+
 	});
 }
 
-function mouseJump(targetElement) {
-	//get top coordinate of clicked muni station div
-	//shift position 15 pixels to the right so you center the mouse around the node
-	var latNum = (parseInt($(targetElement).css('top'))-15);
-	var newMouseLatitude = latNum.toString() + "px";
-	//get right coordinate of clicked muni station div
-	//shift position 8 pixels up so you center the mouse around the node
-	var longNum = (parseInt($(targetElement).css('right'))-8);
-	var newMouseLongitude = longNum.toString() + "px";
-	//only move mouse if mouse and new muni stop are part of the same line
+function completeMove(targetElement) {
+	//if mouse and new muni station are part of the same line, move mouse and change its classes
 	if (($('#mouse').hasClass('redLine') && $(targetElement).hasClass('redLine')) ||
 		($('#mouse').hasClass('orangeLine') && $(targetElement).hasClass('orangeLine')) ||
 		($('#mouse').hasClass('yellowLine') && $(targetElement).hasClass('yellowLine')) ||
 		($('#mouse').hasClass('greenLine') && $(targetElement).hasClass('greenLine')) ||
 		($('#mouse').hasClass('blueLine') && $(targetElement).hasClass('blueLine')) ||
 		($('#mouse').hasClass('purpleLine') && $(targetElement).hasClass('purpleLine'))) {
-			//set mouse position so that it now appears at the clicked muni station
-			$('#mouse').css({'top' : newMouseLatitude, 'right' : newMouseLongitude});
-			//create variable that holds all classes from new muni stop, which we will later assign to mouse
-			var classList = targetElement.classList.toString();
-			//remove all previously set classes from mouse with .removeClass()
-			$('#mouse').removeClass();
-			//add line classes of new location to mouse with .addClass()
-			$('#mouse').addClass(classList);
-			//get rid of wrong move text decorations and exclamation point
+			moveMouse(targetElement);
+			adjustClasses(targetElement);
+			//get rid of invalid move alerts
 			$('#invalidMove').css({'color' : 'black', 'font-size' : '16px'});
 			$('#exclamationPointDiv').css({'display' : 'none'});
-	//else if they do not belong to the same line, alert that they must go to transfer station
+	//else if they do not belong to the same line, alert that they go to transfer station
 	} else {
+		invalidMoveAlert();
+	}
+} 
+
+function moveMouse(targetElement) {
+	//set mouse position so that it hovers over desired muni station
+	var newMouseLatitude = (parseInt($(targetElement).css('top'))-15).toString() + "px";
+	var newMouseLongitude = (parseInt($(targetElement).css('right'))-8).toString() + "px";
+	$('#mouse').css({'top' : newMouseLatitude, 'right' : newMouseLongitude});
+}
+
+function adjustClasses(targetElement) {
+			var classList = targetElement.classList.toString();
+			$('#mouse').removeClass();
+			$('#mouse').addClass(classList);
+			$('#mouse').removeClass('munistop');
+}
+
+function invalidMoveAlert() {
 		//turn text in instructions red
 		$('#invalidMove').css({'color' : '#EA242F', 'font-size' : '16px'});
 		//have an exclamation point pop up over the mouse to alert an invalid move
-		var currentMouseLatitude = (parseInt($('#mouse').css('top'))-42);
-		var exclamationPointLatitude = currentMouseLatitude.toString() + 'px';
-		console.log(exclamationPointLatitude);
-		var currentMouseLongitude = (parseInt($('#mouse').css('right')) + 2);
-		var exclamationPointLongitude = currentMouseLongitude.toString() + 'px';
-		console.log(exclamationPointLongitude);
+		var exclamationPointLatitude = (parseInt($('#mouse').css('top'))-42).toString() + "px";
+		var exclamationPointLongitude = (parseInt($('#mouse').css('right'))).toString() + "px";
 		$('#exclamationPointDiv').css({'display' : 'block', 'top': exclamationPointLatitude, 'right' : exclamationPointLongitude});
-	}
-
 }
+
+
+
+	// function getRandomAdventure(){
+	// 	var min = 0;
+	// 	var max = arrayOfFunctions.length;
+	// 	arrayOfFunctions[(Math.floor(Math.random() * (max-min)) + min)]();
+	// }
+	// getRandomAdventure();
