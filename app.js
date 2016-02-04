a0873a234ab58ac71795733280aceca0d85870d0
jessicaahong <jessiehong9@gmail.com>
2016-02-04 13:20:25 -0800

    makes game winnable, adds features so you cannot move mouse after win, displays winner message

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 function init(){
 	$('button').on('click', newGame);
+	//make nodes clickable and game winnable
+	loadClickEventListeners();
 }
-
+// global variables
 var hasStarted = false;
+var hasWon = false;
 var arrayOfFunctions = [
 			setBeerAdventure,
 			setBrownieAdventure,
@@ -19,14 +22,14 @@ var arrayOfFunctions = [
 			setSushiAdventure];
 var winningStation;
 var currentStation;
+
 function newGame() {
-	hasStarted=true;
+	hasStarted = true;
+	hasWon = false;
 	//make exclamation point invisible with new game
 	$('#exclamationPointDiv').css({'display' : 'none'});
 	//make all food icons invisible with new game
 	$('.food').css({'display': 'none'});
-	//make nodes clickable and game winnable
-	loadClickEventListeners();
 	//select new adventure
 	selectNewAdventure();
 	//display mouse at new location
@@ -34,33 +37,27 @@ function newGame() {
 }
 
 
-
-
-//functions that make mouse mobile
 function loadClickEventListeners() {
 	$('.munistop').on('click', function() {
 		letMouseWin(event.target);
-		completeMove(event.target);
+		if (!hasWon) {
+			completeMove(event.target);
+		}
 	});
 }
 
 function letMouseWin(targetElement){
 	var currentStation = targetElement;
-	var winningStation = document.querySelector('#japantown');
-	console.log(currentStation);
-	console.log(winningStation);
-	//sets a winner Node
-	setWinningStation();
 	if (currentStation == winningStation) {
+		moveMouse(targetElement);
+		$('.food').css({'display' : 'none'});
 		showWinnerMessage();
+		hasWon = true;
 	}
-}
-function setWinningStation() {
-	var winningStation = document.querySelector('#japantown');
 }
 
 function showWinnerMessage(){
-	console.log('we have a winner');
+	$('#adventureDetails').html("Congratulations! You have won the game!<br>Click <a href='#'>here</a> to find out more about restaurants in the neighborhood!<br>Click new game to send your mouse on another adventure!");
 }
 
 function completeMove(targetElement) {
@@ -134,78 +131,91 @@ function selectNewAdventure() {
 
 function setBeerAdventure() {
 	$('#adventureDetails').html('Direct mouse to dogpatch to grab some beers!');
+	winningStation = document.querySelector('#dogpatch');	
 	moveMouse('#nineteenthSt');
 	adjustClasses(document.querySelector('#nineteenthSt'));
 	$('#beer').css({'display': 'block'});
 }
 function setBrownieAdventure() {
 	$('#adventureDetails').html('Direct mouse to the Haight to pick up some brownies!');
+	winningStation = document.querySelector('#haightAshbury');
 	moveMouse('#hudson');
 	adjustClasses(document.querySelector('#hudson'));
 	$('#brownie').css({'display': 'block'});
 }
 function setBurgerAdventure() {
 	$('#adventureDetails').html('Direct mouse to dividadero street for a NOPA burger!');
+	winningStation = document.querySelector('#divisadero');
 	moveMouse('#glenPark');
 	adjustClasses(document.querySelector('#glenPark'));
 	$('#burger').css({'display': 'block'});
 }
 function setBurritoAdventure() {
 	$('#adventureDetails').html('Direct mouse to 24th St for la taqueria!');
+	winningStation = document.querySelector('#twentyFourthSt');
 	moveMouse('#landsEnd');
 	adjustClasses(document.querySelector('#landsEnd'));
 	$('#burrito').css({'display': 'block'});
 }
 function setCheeseAdventure() {
 	$('#adventureDetails').html('Direct mouse to 16th St to get some cheese at BiRite!');
+	winningStation = document.querySelector('#sixteenthSt');
 	moveMouse('#union');
 	adjustClasses(document.querySelector('#union'));
 	$('#cheese').css({'display': 'block'});
 }
 function setChowderAdventure() {
 	$('#adventureDetails').html('Direct mouse to the wharf for some clam chowder!');
+	winningStation = document.querySelector('#wharf');
 	moveMouse('#bayview');
 	adjustClasses(document.querySelector('#bayview'));
 	$('#chowder').css({'display': 'block'});
 }
 function setCoffeeAdventure() {
 	$('#adventureDetails').html('Direct mouse the castro for a cup of coffee!');
+	winningStation = document.querySelector('#castro');
 	moveMouse('#union');
 	adjustClasses(document.querySelector('#union'));
 	$('#coffee').css({'display': 'block'});
 }
 function setCrabAdventure() {
 	$('#adventureDetails').html('Direct mouse Land\'s End for some seafood!');
+	winningStation = document.querySelector('#landsEnd');	
 	moveMouse('#designDistrict');
 	adjustClasses(document.querySelector('#designDistrict'));
 	$('#crab').css({'display': 'block'});
 }
 function setDimsumAdventure() {
 	$('#adventureDetails').html('Direct mouse the richmond for some dim sum!');
+	winningStation = document.querySelector('#theAvenues');	
 	moveMouse('#civicCenter');
 	adjustClasses(document.querySelector('#civicCenter'));
 	$('#dimsum').css({'display': 'block'});
 }
 function setHotdogAdventure() {
 	$('#adventureDetails').html('Direct mouse to the ballpark for a hot dog!');
+	winningStation = document.querySelector('#fourthAndKing');
 	moveMouse('#balboaPark');
 	adjustClasses(document.querySelector('#balboaPark'));
 	$('#hotdog').css({'display': 'block'});
 }
 function setPizzaAdventure() {
 	$('#adventureDetails').html('Direct mouse to North Beach for a slice of pizza!');
+	winningStation = document.querySelector('#northBeach');
 	moveMouse('#taraval');
 	adjustClasses(document.querySelector('#taraval'));
 	$('#pizza').css({'display': 'block'});
 }
 function setPretzelAdventure() {
 	$('#adventureDetails').html('Direct mouse to Stonestown Galleria for a soft pretzel!');
+	winningStation = document.querySelector('#stonestown');
 	moveMouse('#candlestick');
 	adjustClasses(document.querySelector('#candlestick'));
 	$('#pretzel').css({'display': 'block'});
 }
 function setSushiAdventure(){
 	$('#adventureDetails').html('Direct mouse to Japantown for some sushi!');
+	winningStation = document.querySelector('#japantown');
 	moveMouse('#stonestown');
 	adjustClasses(document.querySelector('#stonestown'));
 	$('#sushi').css({'display' : 'block'});	
